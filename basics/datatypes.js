a614a40ef732c8618a90f1d4c619faf781945016
Mudit Dargar <115471612+MuditDargar@users.noreply.github.com>
2023-12-26 12:39:00 +0530

    Add files via upload

---
+++ basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello"); // we are using nodejs ,not browser
+
+console.log(3+3+12) //code readaibility is high
+
+console.log("miindians")
+
+let name="mudiit" //string datatype
+let age=20  // integer datatype
+let login= true //boolean datatype
+
+/* PRIMITVE */
+// number => 2 to power 53
+//bigint 
+//string=> " "
+//boolean =>true and false
+// null => standalone value
+//undefined =>
+//symbols =>unique 
+
+
+/* SECONDARY */
+//object
+
+//typeof use for indentify the type of data types
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof login)
+
+console.log(typeof null) //type of null is object
+console.log(typeof undefined) //tpye of undefined is undefined
---
+++ basics/variable.js
@@ -0,0 +1,21 @@
+let accountId="12212"
+var name="Mudit dargar"
+const roll_data=21221
+account_holder="powelldarry"
+let mudit ;
+
+accountId="22222"
+
+console.log(name)
+
+name="sachin"
+account_holder=232323
+// roll_data=2222 //not allowed
+/* 
+prefer to use let and const .not var because of issue in block space and function scope
+*/
+
+console.table([name,account_holder,accountId,roll_data,mudit])
+
+
+
